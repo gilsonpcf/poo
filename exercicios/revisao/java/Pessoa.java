3084b9cd9c538ebbf01982843b3e1a1a514683af
Gilson Pereira <gilsonpcf@gmail.com>
2022-04-27 17:04:16 -0300

    Update Pessoa.java

--- exercicios/revisao/java/Pessoa.java
+++ exercicios/revisao/java/Pessoa.java
@@ -1,12 +1,22 @@
-//import java.util.Calendar;
-import java.util.Date;
-import java.text.SimpleDateFormat;
+import java.util.Calendar;
+//import java.util.Date;
+//import java.text.SimpleDateFormat;
 
 public class Pessoa {
 	private String nome;
 	private String dataNascimento;
 	private double altura;
 
+	public Pessoa() {
+
+	}
+	
+	public Pessoa(String nome, String dataNascimento, double altura) {
+		this.nome = nome;
+		this.dataNascimento = dataNascimento;
+		this.altura = altura;
+	}
+	
 	public String getNome() {
 		return nome;
 	}
@@ -39,10 +49,10 @@ public class Pessoa {
 	}
 
 	public int calculaIdade() {
-		//int anoAtual = Calendar.getInstance().get(Calendar.YEAR);
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy");
-		Date date = new Date();
-		int anoAtual = Integer.parseInt(dateFormat.format(date));
+		//SimpleDateFormat formatoData = new SimpleDateFormat("yyyy");
+		//Date data = new Date();
+		//int anoAtual = Integer.parseInt(formatoData.format(data));
+		int anoAtual = Calendar.getInstance().get(Calendar.YEAR);
 
 		int anoNascimento = Integer.parseInt(dataNascimento.substring(6, 10));
 
