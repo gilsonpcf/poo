0252da8d73c7059ffce5cc866c9999da21e0431d
Gilson Pereira <gilsonpcf@gmail.com>
2022-04-27 17:38:25 -0300

    Update Elevador.java

--- exercicios/revisao/java/Elevador.java
+++ exercicios/revisao/java/Elevador.java
@@ -1,88 +1,81 @@
 public class Elevador {
-    private int andarAtual;
-    private int totalAndares;
-    private int capacidade;
-    private int qtdPessoas;
+	private int andarAtual;
+	private int totalAndares;
+	private int capacidade;
+	private int pessoasPresentes;
 
-    public Elevador(int capacidade, int totalAndares) {
-        inicializa(capacidade, totalAndares);
-        imprimeDados();
-    }
-
-    public void inicializa(int capacidade, int totalAndares) {
-        this.capacidade = capacidade;
-        this.totalAndares = totalAndares;
-        this.andarAtual = 0; // Térreo
-        this.qtdPessoas = 0; // Elevador vazio
-    }
-
-    public void entra() {
-        if (qtdPessoas < capacidade) {
-            qtdPessoas++;
-        }
-
-        imprimeDados();
-    }
-
-    public void sai() {
-        if (qtdPessoas > 0) {
-            qtdPessoas--;
-        }
-
-        imprimeDados();
-    }
-
-    public void sobe() {
-        if (andarAtual < totalAndares) {
-            andarAtual++;
-        }
-
-        imprimeDados();
-    }
-
-    public void desce() {
-        if (andarAtual > 0) {
-            andarAtual--;
-        }
-
-        imprimeDados();
-    }
-
-    public int getAndarAtual() {
-        return andarAtual;
-    }
-
-    public void setAndarAtual(int andarAtual) {
-        this.andarAtual = andarAtual;
-    }
-
-    public int getTotalAndares() {
-        return totalAndares;
-    }
-
-    public void setTotalAndares(int totalAndares) {
-        this.totalAndares = totalAndares;
-    }
-
-    public int getCapacidade() {
-        return capacidade;
-    }
-
-    public void setCapacidade(int capacidade) {
-        this.capacidade = capacidade;
-    }
-
-    public int getQtdPessoas() {
-        return qtdPessoas;
-    }
-
-    public void setQtdPessoas(int qtdPessoas) {
-        this.qtdPessoas = qtdPessoas;
-    }
-
-    public void imprimeDados() {
-        System.out.println("Andar: " + andarAtual);
-        System.out.println("Pessoas: " + qtdPessoas);
-        System.out.println();
-    }
+	public Elevador(int capacidade, int totalAndares) {
+		inicializa(capacidade, totalAndares);
+	}
+	
+	private void inicializa(int capacidade, int totalAndares) {
+		this.capacidade = capacidade;
+		this.totalAndares = totalAndares;
+		this.andarAtual = 0;
+		this.pessoasPresentes = 0;
+	}
+	
+	public void entra() {
+		if (pessoasPresentes < capacidade) {
+			pessoasPresentes++;
+		} else {
+			System.out.println("Elevador lotado!");
+		}
+	}
+	
+	public void sai() {
+		if (pessoasPresentes > 0) {
+			pessoasPresentes--;
+		} else {
+			System.out.println("Elevador vazio!");
+		}
+	}
+	
+	public void sobe() {
+		if (andarAtual < totalAndares) {
+			andarAtual++;
+		} else {
+			System.out.println("Elevador está no último andar!");
+		}
+	}
+	
+	public void desce() {
+		if (andarAtual > 0) {
+			andarAtual--;
+		} else {
+			System.out.println("Elevador está no térreo!");
+		}
+	}
+	
+	public int getAndarAtual() {
+		return andarAtual;
+	}
+	
+	public int getTotalAndares() {
+		return totalAndares;
+	}
+	
+	public int getCapacidade() {
+		return capacidade;
+	}
+	
+	public int getPessoasPresentes() {
+		return pessoasPresentes;
+	}
+	
+	public void setAndarAtual(int andarAtual) {
+		this.andarAtual = andarAtual;
+	}
+	
+	public void setTotalAndares(int totalAndares) {
+		this.totalAndares = totalAndares;
+	}
+	
+	public void setCapacidade(int capacidade) {
+		this.capacidade = capacidade;
+	}
+	
+	public void setPessoasPresentes(int pessoasPresentes) {
+		this.pessoasPresentes = pessoasPresentes;
+	}
 }
